be8a6e5db36d931af2f7e233d413a47ad06de08b
Kebalepile <pimpabutterfly86@gmail.com>
2024-08-14 16:55:15 +0200

    create pagination component

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './pagination.css'
+
+function Pagination({ currentPage, totalPages, onPageChange }) {
+  const generatePageNumbers = () => {
+    const pageNumbers = []
+    const startPage = Math.max(1, currentPage - 1)
+    const endPage = Math.min(totalPages, startPage + 3)
+
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(i)
+    }
+
+    return pageNumbers
+  }
+
+  return (
+    <div className='pagination'>
+      <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+        Previous
+      </button>
+      {generatePageNumbers().map(page => (
+        <button
+          key={page}
+          className={page === currentPage ? 'active' : ''}
+          onClick={() => onPageChange(page)}
+        >
+          {page}
+        </button>
+      ))}
+      <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
+  )
+}
+
+export default Pagination
