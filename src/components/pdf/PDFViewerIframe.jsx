c17c7998938bae658ef906a2b1ffd7b0d1b0d93a
Kebalepile <pimpabutterfly86@gmail.com>
2024-08-15 11:44:51 +0200

    fix pdf view button toggle z-index probmlen

--- src/components/pdf/PDFViewerIframe.jsx
+++ src/components/pdf/PDFViewerIframe.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import usePdfIframe from '../../hooks/usePdfIframe';
-import useLoadingPlaceholder from '../../hooks/useLoadingPlaceholder';
 import PropTypes from 'prop-types';
 import './pdf_viewer.css';
 
@@ -11,40 +10,56 @@ import './pdf_viewer.css';
  *
  * @param {Object} props - The component props.
  * @param {string} props.file - The URL or path of the PDF file to be displayed in the iframe.
- * 
+ *
  * @returns {JSX.Element} A div element containing the PDF viewer in an iframe or a loading placeholder, with a button to toggle the view mode.
  */
 const PDFViewerIframe = ({ file }) => {
   const iframeUrl = usePdfIframe(file);
   const [isFullView, setIsFullView] = useState(false);
-  const [isPdfContainerViewerLoaded] = useLoadingPlaceholder(5000);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000); // 3 seconds delay
+
+    return () => clearTimeout(timer); // Cleanup the timer if the component unmounts
+  }, []);
 
   const toggleView = () => setIsFullView(!isFullView);
 
   return (
-    <div className={`pdf-viewer-container ${isFullView ? 'full-view' : 'min-view'}`}>
-      <button className="toggle-view-btn" onClick={toggleView}>
-        {isFullView ? 'Minimize View' : 'Full View'}
-      </button>
-      {isPdfContainerViewerLoaded ? (
-        iframeUrl ? (
+    <div
+      className={`pdf-viewer-container ${
+        isFullView ? 'full-view' : 'min-view'
+      }`}
+    >
+      {isLoading ? (
+        <div className="placeholder pdf-viewer-container-placeholder"></div>
+      ) : (
+        <>
+          <button
+            className={`toggle-view-btn ${
+              isFullView ? 'full-view-btn' : 'min-view-btn'
+            }`}
+            onClick={toggleView}
+          >
+            {isFullView ? 'Minimize View' : 'Full View'}
+          </button>
+
           <iframe
             src={iframeUrl}
-            className="pdf-viewer-iframe"
-            title="PDF Viewer"
+            className='pdf-viewer-iframe'
+            title='PDF Viewer'
           />
-        ) : (
-          <p>Loading...</p>
-        )
-      ) : (
-        <div className="placeholder pdf-viewer-container-placeholder"></div>
+        </>
       )}
     </div>
   );
-};
+}
 
 PDFViewerIframe.propTypes = {
-  file: PropTypes.string.isRequired,
+  file: PropTypes.string.isRequired
 };
 
 export default PDFViewerIframe;
