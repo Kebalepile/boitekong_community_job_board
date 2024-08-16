18088ee93af74e6c066bc4cc518705dfe37d9683
Kebalepile <pimpabutterfly86@gmail.com>
2024-08-16 15:07:31 +0200

    version 3 prodction test ok

--- web_scrapers/main.py
+++ web_scrapers/main.py
@@ -1,4 +1,4 @@
-from spiders.public.govpage.govpageSpider import Spider as PublicSpider
+# from spiders.public.govpage.govpageSpider import Spider as PublicSpider
 # from spiders.private.govpage.govpageSpider import Spider as PrivateSpider
 # from bots.entities.national.national_credit_regulator.Bot import Bot as NCR_Bot
 # from  bots.entities.national.community_schemes_ombud_services.Bot import Bot as CSOS_Bot
@@ -10,15 +10,15 @@ import logging
 
 # from utils.rename_imgs_and_generate_json import rename_images_and_create_json
 # from utils.rename_pdfs_and_generate_json import rename_pdfs_and_create_json
-# from utils.pdf_to_image import create_images_from_pdf_pages
+from utils.pdf_to_image import create_images_from_pdf_pages
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%d %B %Y %H:%M:%S')
 
     try:
-        logging.info("Starting Public Spider")
-        govpage_spider = PublicSpider()
-        govpage_spider.launch()
+        # logging.info("Starting Public Spider")
+        # govpage_spider = PublicSpider()
+        # govpage_spider.launch()
 
 
         # logging.info("Pausing for 10 seconds")
@@ -44,7 +44,7 @@ def main():
 
         # rename_images_and_create_json()
         # rename_pdfs_and_create_json()
-        # create_images_from_pdf_pages()
+        create_images_from_pdf_pages()
     except Exception as e:
         logging.error(f"An error occurred while initiating bots: {e}")
 
